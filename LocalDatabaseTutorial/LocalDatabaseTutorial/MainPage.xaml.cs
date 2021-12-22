2bc620494ca73582daaf4efb6396c6e60156da80
Uzzi Yxung <yxnguzzi@gmail.com>
2021-12-23 00:14:46 +0300

    Добавьте файлы проекта.

---
+++ LocalDatabaseTutorial/LocalDatabaseTutorial/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocalDatabaseTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            collectionView.ItemsSource = await App.Database.GetPeopleAsync();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(nameEntry.Text) && !string.IsNullOrWhiteSpace(ageEntry.Text))
+            {
+                await App.Database.SavePersonAsync(new Person
+                {
+                    Name = nameEntry.Text,
+                    Age = int.Parse(ageEntry.Text)
+                });
+
+                nameEntry.Text = ageEntry.Text = string.Empty;
+                collectionView.ItemsSource = await App.Database.GetPeopleAsync();
+            }
+        }
+    }
+}
